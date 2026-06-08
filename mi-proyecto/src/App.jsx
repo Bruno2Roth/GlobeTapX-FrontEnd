51d603112f1da58451c655852c27ab4fa6028656
KIARA BRUKIEW <49518961@ort>
2026-06-08 11:35:35 -0300

    arreglando cambio

--- mi-proyecto/src/App.jsx
+++ mi-proyecto/src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./Pages/login";
 import Home from "./Pages/home";
 import Clima from "./Pages/clima";
 import Idioma from "./Pages/idioma";
+import Cambio from "./Pages/cambio";
+
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/clima" element={<Clima />} />
         <Route path="/idioma" element={<Idioma />} />
+        <Route path="/cambio" element={<Cambio />} />
+
       </Routes>
     </BrowserRouter>
   );
